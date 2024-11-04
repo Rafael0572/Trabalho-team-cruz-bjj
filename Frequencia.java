adfa11f9ee3f5aeeda46eddff87a5e0c8dabd3c9
Rafael <rafael._.gouveia@hotmail.com>
2024-11-04 12:18:57 -0300

    Add files via upload

---
+++ Frequencia.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+public class Frequencia {
+    private Aluno aluno;
+    private Date data;
+    public Frequencia(Aluno aluno) {
+        this.aluno = aluno;
+        this.data = new Date(); // Data atual
+    }
+    public Aluno getAluno() {
+        return aluno;
+    }
+    public Date getData() {
+        return data;
+    }
+}
